689f0b603b1c85bb382bbe0484a22ede6f884b58
aglaiabox <aglaiabox@gmail.com>
2021-01-15 23:16:59 +0200

    123

--- src/ru/Main.java
+++ src/ru/Main.java
@@ -5,37 +5,35 @@ package ru;
 //  там повторяются слова, надо почситать сколько раз какое слово повторяется.
 //  ЗАДАНИЕ НА МАПЫ. сделать два варианта: с мапой и без
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
    public static void main(String[] args) {
       System.out.println("введите слова");
       String str = "I have a hare I have a bear My toys are here My toys are there";
       String[] wordsArray= str.split(" ");
-      ArrayList<String> listWords = new ArrayList<String>();
+      ArrayList<String> listWords = new ArrayList<>();
+      Map<String,Integer> mapCountWords= new HashMap<>();
+
+      LinkedList<Integer> listCounts= new LinkedList<>();
+
+      Integer a1 = 0;
+      listCounts.add(a1);
+      a1++;
+      mapCountWords.put(wordsArray[0], listCounts.get(0));
+      for (int i = 1; i < wordsArray.length; i++) {
+         if () // через мапу проверяем есть ли в мапе это значение, если нет - добавляем новое
+      }
+
 
       for (String word : wordsArray) {
          listWords.add(word);
       }
       System.out.println(listWords);
 
-      // способ без мапы: коипруем наш массив в другой массив
-      for (int i = 0; i < wordsArray.length; i++) {
-         int n = wordsArray.length-i;
-         int quantity = 1;
 
-            for (int j = 1; j < n; j++) {
-               if (wordsArray[i] != null) {
-                  if (wordsArray[i] == wordsArray[j]) {
-                     quantity++;
-                     wordsArray[j] = null;
-                  }
-               }
-            }
-            if (wordsArray[i]!=null) {
-               System.out.println(wordsArray[i] + ": " + quantity + " occasions.");
-            }
-      }
 
-   }}
+   }
+
+}
 
