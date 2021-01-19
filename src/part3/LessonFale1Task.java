b44c7be56c3730aba18ef995b556541fe5b029f5
aglaiabox <aglaiabox@gmail.com>
2021-01-19 08:02:11 +0200

    file3

--- src/part3/LessonFale1Task.java
+++ src/part3/LessonFale1Task.java
@@ -8,39 +8,55 @@ public class LessonFale1Task {
     public static void main (String[] args) throws Exception {
 
         String nameFile = "/Users/aglaia/Documents/poem.txt";
+        String nameFile2 = "/Users/aglaia/Documents/poem2.txt";
 
-        inputStream(nameFile);
-        inputBuffered(nameFile);
+        long timeInputStream = inputStream(nameFile);
+        long timeInputBuffered = inputBuffered(nameFile2);
 
 
+        String str = "time buffered = "+timeInputBuffered +", time stream = "+timeInputStream;
+        addStringToDoc(str);
+        System.out.println(str);
+
+// как сделать перенос строки когда записываешь в файл?
     }
 
-    private static void inputStream(String nameFile) throws IOException{
+    public static void countTime(){
+        // можно ли сделать метод который принимает в качестве аргумента другой метод - чтобы посчитать его время
+    }
+
+    public static long inputStream(String nameFile) throws IOException{
         long start = System.currentTimeMillis();
         File file = new File(nameFile); // what is it?
         FileInputStream fileInput = new FileInputStream(file);
         int i;
 
+        StringBuilder stringBuilder = new StringBuilder();
         while((i=fileInput.read())!= -1){
-            System.out.print((char)i);
+            stringBuilder.append((char)i);
         }
-        System.out.println();
+        System.out.println(stringBuilder);
         long finish = System.currentTimeMillis();
+        System.out.print("time inputStream: ");
         System.out.println(finish-start);
+        return finish-start;
     }
 
-    private static void inputBuffered(String nameFile) throws IOException{
+    public static long inputBuffered(String nameFile) throws IOException{
         long start = System.currentTimeMillis();
         File file = new File((nameFile));
         BufferedInputStream fileInput = new BufferedInputStream(new FileInputStream(file));
         int i;
 
+        StringBuilder stringBuilder = new StringBuilder();
         while((i=fileInput.read())!= -1){
-            System.out.print((char)i);
+            stringBuilder.append((char)i);
         }
-        System.out.println();
+        System.out.println(stringBuilder);
         long finish = System.currentTimeMillis();
+        System.out.print("time inputBuffered: ");
         System.out.println(finish-start);
+        return finish-start;
     }
 
     public static void createNewDoc(String name) throws FileNotFoundException {
@@ -48,6 +64,12 @@ public class LessonFale1Task {
         File file = new File(wayAndName);
         FileOutputStream fos = new FileOutputStream(file, true);
 
+    }
+
+    public static void addStringToDoc(String str) throws IOException{
+        FileOutputStream fos = new FileOutputStream("/Users/aglaia/Documents/test.s", true);
+        fos.write(str.getBytes());
+        fos.close();
     }
 
 
--- src/part3/LessonFile2Task.java
+++ src/part3/LessonFile2Task.java
@@ -1,15 +1,121 @@
 package part3;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.LinkedList;
 
 public class LessonFile2Task {
 
-    //todo read file with some russian text using FileInputSreame
-    //todo read file with some russian text using BufferedReader
-    //todo add stop word to text and read
-    //todo read all file to list of string (by lines)
-    //todo read all file to 1 string
-    public static void main(String[] args) throws IOException {
+
+
+
+    public static void main(String[] args) throws Exception {
+        //todo read file with some russian text using FileInputSreame
+        //todo read file with some russian text using BufferedReader
+//        inputBuffered("russianpoem");
+//        inputStream("poem.txt");
+
+
+        //todo read all file to list of string (by lines)           Done
+        //todo read all file to 1 string                            Done
+//        String a = readToString("trains.s");
+//        System.out.println(a);
+//        LinkedList<String> list = readToList("trains.s");
+//        System.out.println(list);
+
+        //todo add stop word to text and read
+String textUntilStopWord = readUntilStopWord("poem3.txt", "playhouse");
+        System.out.println(textUntilStopWord);
+        }
+        // The proper exception handling - what is it?
+
+        private static String readUntilStopWord (String name, String stopWord) throws Exception{
+        String wayAndName = "/Users/aglaia/Documents/"+name;
+        InputStream inputStream = new FileInputStream(wayAndName);
+        Reader isr = new InputStreamReader(inputStream);
+        int i = isr.read();
+        while (i !=-1){
+            char ch = (char) i;
+            while (i !=)
+            i = isr.read(); // почему это можно здесь, а можно засунуть в усоловие while
+        }
+        isr.close();
+
+//        BufferedReader bf = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+//            String s;
+//            StringBuilder line = new StringBuilder();
+//            StringBuilder word = new StringBuilder();
+//            String w = null;
+//            String textUntilStopWord = null;
+//            while ((s = bf.readLine()) != null) {
+//                if (s != " ") {
+//                    word.append(s);
+//                    w = word.toString();
+//                    System.out.println(w);
+//                    System.out.println("//");
+//                } else if (s == " "){
+//                    if (w != stopWord) {
+//                        line.append(w);
+//                        line.append(' ');
+//                    } else {
+//                        line.append(w);
+//                        textUntilStopWord = line.toString();
+//                        break;
+//
+//                    }
+//                }
+//            }
+//return textUntilStopWord;
+        }
+
+    public static void inputBuffered (String name) throws Exception {
+        String wayAndName = "/Users/aglaia/Documents/" + name;
+        File file = new File(wayAndName);
+        BufferedReader bf = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+        String s; //почему стринг а не инт?
+        while ((s = bf.readLine()) != null) {
+            System.out.println(s); // почему println печатает не букву на строчку?
+        }
+    }
+
+
+    private static void inputStream (String name) throws Exception{
+        String wayAndName = "/Users/aglaia/Documents/"+name;
+        File file = new File(wayAndName);
+        FileInputStream fis = new FileInputStream(file);
+        int i;
+        while ((i = fis.read()) != -1){
+            System.out.print((char) i);
+        }
+    }
+
+
+
+    public static String readToString (String name) throws Exception {
+        String wayAndName = "/Users/aglaia/Documents/" + name;
+        File file = new File(wayAndName);
+        BufferedReader bf = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+        String s;
+        StringBuilder line = new StringBuilder();
+        while ((s = bf.readLine()) != null) {
+            line.append(s);
+            line.append('\n');
+        }
+        String res = line.toString();
+        // как добавить пробелы между строчками?
+        return res;
+    }
+
+    public static LinkedList readToList (String name) throws Exception{
+        String wayAndName = "/Users/aglaia/Documents/" + name;
+        File file = new File(wayAndName);
+        BufferedReader bf = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+        LinkedList<String> list= new LinkedList<>();
+        String s;
+        while ((s = bf.readLine()) != null){
+            list.add(s);
+        }
+        return list;
+    }
 
     }
-}
+
