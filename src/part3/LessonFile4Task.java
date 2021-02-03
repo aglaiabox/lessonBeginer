346877edb45a8c346dc86c8831e2269c7f777cf4
aglaiabox <aglaiabox@gmail.com>
2021-02-03 20:21:28 +0200

    ex3

--- src/part3/LessonFile4Task.java
+++ src/part3/LessonFile4Task.java
@@ -4,11 +4,84 @@ package part3;
 //  отсортировать в алфавитном порядке и
 //  сохранить в другой файл
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 //todo читаю текст из файла и делаю словарь -
 // чтобы пользователь вводит букву и
 // получает все слова на эту букву. Использовать hash map
 public class LessonFile4Task {
-    public static void main(String[] args){
+    public static Map<Character, ArrayList<String>> mapa = new HashMap<>();
+    public static void main(String[] args) throws Exception{
+        String text = readFile("poem.txt");
+        String text1 = deleteDoubleSpases(text);
+        System.out.println(text);
+        addWordsToMapa(text1);
 
+        System.out.println("Enter a letter");
+        Scanner scan = new Scanner(System.in);
+        String st = scan.next();
+        char ch = st.charAt(0);
+        System.out.println( mapa.get(ch));
+    }
+
+    private static void addWordsToMapa(String text) {
+        String [] words = text.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            String word = words [i];
+            char firstLetter = word.charAt(0);
+            int indexLastLetter = word.length()-1;
+            char lastSymbol = word.charAt(indexLastLetter);
+            if (lastSymbol == ','|| lastSymbol == '.'||lastSymbol == ':'||lastSymbol == ';'||lastSymbol == '"'||lastSymbol == '-' ){
+                word = word.substring(0,indexLastLetter);
+            }
+            if (!mapa.containsKey(firstLetter)){
+                ArrayList<String> listWordsOneFirstLetter = new ArrayList<>();
+                listWordsOneFirstLetter.add(word);
+                mapa.put(firstLetter,listWordsOneFirstLetter);
+                System.out.println("New letter " +firstLetter+" was created. "+"Word "+word+" was added to dictionary. ("+i+"/"+words.length );
+            } else {
+                ArrayList<String>listWordsOneFirstLetter = mapa.get(firstLetter);
+                listWordsOneFirstLetter.add(word);
+                System.out.println("Letter " +firstLetter+" even exist. "+"Word "+word+" was added to dictionary. ("+i+"/"+words.length);
+
+            }
+        }
+    }
+
+    private static String readFile(String name) throws IOException {
+        File file = new File(PathForLessonsFiles.path + name);
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        StringBuilder stringBuilder = new StringBuilder();
+        String st;
+        while ((st = br.readLine()) != null){
+        stringBuilder.append(st);
+//        stringBuilder.append(" ");
+        }
+        String text = stringBuilder.toString();
+        return text;
+    }
+
+    private static String deleteDoubleSpases (String text){
+        StringBuffer stringBuffer = new StringBuffer(text);
+        for (int i = 0; i < stringBuffer.length(); i++) {
+            int iNext = i+1;
+            char ch = stringBuffer.charAt(i);
+            if (ch == ' '){
+                char chNext = stringBuffer.charAt(iNext);
+                if (chNext == ' '){
+                    stringBuffer.deleteCharAt(iNext);
+                }
+
+            }
+        }
+        text = stringBuffer.toString();
+        return text;
     }
 }
